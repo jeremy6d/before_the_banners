61f9a8ae476bf2f9eb952c65c3358240b156d09d
Jeremy Weiland <jeremy6d@gmail.com>
2014-07-08 10:51:26 -0400

    basic auth for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  http_basic_authenticate_with :name => "btb", :password => "leanasfuck", :if => Proc.new { Rails.env.production? }
   before_filter :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
