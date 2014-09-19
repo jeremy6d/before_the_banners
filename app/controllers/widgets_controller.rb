59dccd44fde031f683939930514a441b0fcd6e2e
Jeremy Weiland <jeremy6d@gmail.com>
2014-09-18 21:18:28 -0400

    widgets

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,6 +1,13 @@
 class WidgetsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  
   layout false
+
   def basic
     @project = Project.find params[:project_id]
+    respond_to do |format|
+      format.js
+    end
   end
 end
