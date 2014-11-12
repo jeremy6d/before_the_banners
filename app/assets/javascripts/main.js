adccd92322fc49a437c313e67d10ac3afeb3107a
Gabe Kelley <hello@gabekelley.com>
2014-11-12 15:21:36 -0500

    Way better header image, this may work for forever

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,28 @@
      animationTime = 1200, // time in milliseconds
      contentTop = [];
 
+CanvasImage = function (e, t) {
+  this.image = t;
+  this.element = e;
+  e.width = t.width;
+  e.height = t.height;
+  this.context = e.getContext("2d");
+  this.context.drawImage(t, 0, 0);
+};
+
+CanvasImage.prototype = {
+  blur:function(e) {
+    this.context.globalAlpha = 0.5;
+    for(var t = -e; t <= e; t += 2) {
+      for(var n = -e; n <= e; n += 2) {
+        this.context.drawImage(this.element, n, t);
+        var blob = n >= 0 && t >= 0 && this.context.drawImage(this.element, -(n -1), -(t-1));
+      }
+    }
+  }
+};
+
+
 $(document).ready(function(){ 
 
   // Stop animated scroll if the user does something
@@ -41,6 +63,20 @@ $(document).ready(function(){
         .eq(i).addClass('selected');
       }
     });
+  });
+
+
+
+  // Blur project header image
+  var image, canvasImage, canvas;
+  $(".header-image").each(function() {
+    canvas = this;
+    image = new Image();
+    image.onload = function() {
+      canvasImage = new CanvasImage(canvas, this);
+      canvasImage.blur(5);
+    };
+    image.src = $(canvas).attr("src");
   });
 
 })
