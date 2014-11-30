9d388ca59f356ab3c11d8184b44b50dc5fc7f13f
Gabe Kelley <hello@gabekelley.com>
2014-11-30 12:24:06 -0500

    Added sign up form to homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
   def index
+    @user = User.new
+    @company = @user.build_company
+
     render layout: "homepage"
   end
 end
--- test/helpers/static_helper_test.rb
+++ test/helpers/static_helper_test.rb
@@ -2,8 +2,4 @@ require "test_helper"
 
 describe StaticHelper do
 
-  it "must be a real test" do
-    flunk "Need real tests"
-  end
-
 end
