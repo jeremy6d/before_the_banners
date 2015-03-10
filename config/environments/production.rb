c43b36cfbe3d172d99579cd54d64539792a7956f
Jeremy Weiland <jeremy@jeremyweiland.com>
2015-03-10 00:02:16 -0400

    fix mailer bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.mandrillapp.com',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['MADNRILL_USERNAME'],
+  :address        => "smtp.mandrillapp.com",
+  :port           => "587",
+  :authentication => :login,
+  :user_name      => ENV['MANDRILL_USERNAME'],
   :password       => ENV['MANDRILL_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => 'beforethebanners.com',
   :enable_starttls_auto => true
 }
