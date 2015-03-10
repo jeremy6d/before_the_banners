790cdb27ecd165a2ee1bef550429a17e08d03742
Jeremy Weiland <jeremy@jeremyweiland.com>
2015-03-09 23:47:23 -0400

    upgrade to mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,15 +76,15 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  default_url_options[:host] = "before-the-banners.herokuapp.com"
+  default_url_options[:host] = "beforethebanners.com"
 end
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.mandrillapp.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => ENV['MADNRILL_USERNAME'],
+  :password       => ENV['MANDRILL_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
---
+++ test/features/public_browsing_test.rb
@@ -0,0 +1,47 @@
+require_relative './feature_test_helper'
+
+feature "Public browsing" do
+  before do
+    sign_up! do 
+      @project = create_project! title: "Spatula Warehouse",
+                                 architect_title: "Bubba's Architcture"
+      add_workspaces! "one", "two", "three"
+      @updates = 5.times.map do
+        add_update_to! @project
+      end
+    end
+
+    # ensure not signed in
+    sign_out!
+  end
+
+  scenario "An unauthenticated visitor views the project page from a direct link" do
+    # visitor directly accesses the URL of project page
+    visit project_path(@project)
+
+    # visitor sees project information
+    page.must_have_content @project.title.titleize
+
+    @updates.map(&:body).each do |body|
+      page.must_have_content body.slice(0,50)
+    end
+
+    # visitor does not see admin functions
+    page.wont_have_css("ul.operations.off--page")
+    page.wont_have_content "Add update"
+    page.wont_have_content "Edit"
+  end
+
+  scenario "An unauthenticated visitor searches for a project page" do
+    # visitor navigates to landing page and searches
+    visit root_path
+    click_on "Search now!"
+    fill_in "Terms", with: "bubba"
+    click_on "Search"
+
+    # visitor should see search results
+    page.must_have_content "Spatula Warehouse"
+    click_on "Spatula Warehouse"
+    must_be_on project_path(@project)
+  end
+end
